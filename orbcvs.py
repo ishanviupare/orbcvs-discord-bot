83a8f8c6fcf4fe870cb36a055761c5ae8f4565b0
Ishanvi Upare <ishanviupare@gmail.com>
2026-02-21 01:08:47 -0600

    commented

--- orbcvs.py
+++ orbcvs.py
@@ -3,19 +3,22 @@ import discord # using discord library
 # get token from hidden file
 from dotenv import load_dotenv
 import os
-
 load_dotenv()
 TOKEN = os.getenv("DISCORD_TOKEN")
 
 class Client(discord.Client):
+
+    # bot online, display confirmation
     async def on_ready(self):
         print(f'Logged on as {self.user}!')
 
+    # prints messages in server to console live time
     async def on_message(self, message):
         print(f'Message from {message.author}: {message.content}')
 
+# discord intent permissions
 intents = discord.Intents.default()
 intents.message_content = True
 
 client = Client(intents=intents)
-client.run(TOKEN)
+client.run(TOKEN) # holds token

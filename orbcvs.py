81b6d1fa2d536f5a9725e6a39bcf4979cbe4436e
Ishanvi Upare <ishanviupare@gmail.com>
2026-02-21 01:02:04 -0600

    tutorial vid 1

---
+++ orbcvs.py
@@ -0,0 +1,21 @@
+import discord # using discord library
+
+# get token from hidden file
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+TOKEN = os.getenv("DISCORD_TOKEN")
+
+class Client(discord.Client):
+    async def on_ready(self):
+        print(f'Logged on as {self.user}!')
+
+    async def on_message(self, message):
+        print(f'Message from {message.author}: {message.content}')
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+client = Client(intents=intents)
+client.run(TOKEN)
